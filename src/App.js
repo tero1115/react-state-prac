2fa810e5e064a677c0ec30d6a95e162afd5d810a
tero1115 <limjisang1115@gmail.com>
2023-10-04 15:54:02 +0900

    상태관리 연습 숫자올리기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    // useState 리액트에서 상태를 관리하는 함수
+    // 리턴값은 배열이고
+    // 배열의 첫번째 요소는 값
+    // 배열의 두번째 요소는 값을 변경하는 함수
+    // useState의 매개변수는 값의 초기값
+
+    const [count, setCount] = useState(0);
+
+    const increase = () => {
+        setCount(prev => prev + 1);
+        // setCount(prev => prev + 1);
+        // setCount(count + 1);
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div>{count}</div>
+        <button onClick={increase}>+</button>
     </div>
   );
 }
